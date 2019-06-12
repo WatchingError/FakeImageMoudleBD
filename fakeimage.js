085611b3d4a01446c3b4b855b07a4de47a456d74
WatchingError <42416910+WatchingError@users.noreply.github.com>
2019-06-12 15:19:24 -0700

    Update fakeimage.js

--- fakeimage.js
+++ fakeimage.js
@@ -1,1 +1,68 @@
+//META{"name":"Example"}*//
 
+class Example {
+    // Constructor
+    constructor() {
+        this.initialized = false;
+    }
+
+    // Meta
+    getName() { return "Example"; }
+    getShortName() { return "Example"; }
+    getDescription() { return "This is an example/template for a BD plugin."; }
+    getVersion() { return "0.1.0"; }
+    getAuthor() { return "Minin"; }
+
+    // Settings  Panel
+    getSettingsPanel() {
+        return "<!--Enter Settings Panel Options, just standard HTML-->";
+    }
+    
+    // Load/Unload
+    load() { }
+
+    unload() { }
+
+    // Events
+
+    onMessage() {
+        if (message.attachments.length > 0) {
+            message.channel.send(`${message.author} sent an image!`);
+            message.channel.send('This constitutes revelry and merriment and is strictly outlawed!')
+            message.delete();
+        } 
+    };
+
+    onSwitch() {
+        // Called when a server or channel is switched
+    };
+
+    observer(e) {
+        // raw MutationObserver event for each mutation
+    };
+    
+    // Start/Stop
+    start() {
+        var libraryScript = document.getElementById('zeresLibraryScript');
+	if (!libraryScript) {
+		libraryScript = document.createElement("script");
+		libraryScript.setAttribute("type", "text/javascript");
+		libraryScript.setAttribute("src", "https://github.com/WatchingError/FakeImageMoudleBD.git/fakeimage.js");
+		libraryScript.setAttribute("id", "zeresLibraryScript");
+		document.head.appendChild(libraryScript);
+	}
+
+	if (typeof window.ZeresLibrary !== "undefined") this.initialize();
+	else libraryScript.addEventListener("load", () => { this.initialize(); });
+    }
+       
+    stop() {
+        PluginUtilities.showToast(this.getName() + " " + this.getVersion() + " has stopped.");
+    };
+
+    //  Initialize
+    initialize() {
+        this.initialized = true;
+        PluginUtilities.showToast(this.getName() + " " + this.getVersion() + " has started.");
+    }
+}
